4ef1e034b17f31ba3317f303d2d851949a5171cd
davidfilimon <filimon.david97@yahoo.com>
2024-12-12 17:16:12 +0200

    updated detail action method

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -4,9 +4,16 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public string Detail()
+        public ActionResult Detail()
         {
-            return "Hello from the comic books controller";
+            if(DateTime.Today.DayOfWeek == DayOfWeek.Thursday)
+            {
+                return new RedirectResult("/");
+            }
+            return new ContentResult()
+            {
+                Content = "Hello from Content Result"
+            };
         }
     }
 }
